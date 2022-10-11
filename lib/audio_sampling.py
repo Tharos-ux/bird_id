142dac0b5aaca954e0ec16cb7b8178a3198a34ca
Siegfried Dubois <dubois.siegfried@gmail.com>
2022-10-11 14:59:55 +0200

    Refactoring code audio_sampling

--- lib/audio_sampling.py
+++ lib/audio_sampling.py
@@ -3,40 +3,35 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+from os import listdir
 import os
+from pathlib import Path
 
-def audio_processing(dir):
-    """ Exports raw audios into pre-processed spectrograms """
+def audio_processing(data_path:str) -> None:
+    """Exports raw audios into pre-processed spectrograms
 
-    l_raw_audios = glob(dir)
+    Args:
+        data_path (str): directory containing species folders
+    """
+    for specie in listdir(f"{data_path}/"):
+        for raw_audio in listdir(f"{data_path}/{specie}/"):
+            # cut the audio into chunks
+            l_chunks = load_in_blocks(f"{data_path}/{specie}/{raw_audio}")
+            # creates spectrogram and exports them in
+            export_spectro(l_chunks, specie, raw_audio.split('.')[0])
+            # train_set/<specie_name>/<file_ID>_<spec_nbr>.png
 
-    for raw_audio in l_raw_audios:
-        l_chunks = load_in_blocks(raw_audio) # cut the audio into chunks
-        
-        dir = raw_audio[21:27] # get specie name
-        filename_init = raw_audio[28:-4] # get file ID
-        export_spectro(l_chunks, dir, filename_init) # creates spectrogram and exports them in
-                                             # train_set/<specie_name>/<file_ID>_<spec_nbr>.png
-
-def export_spectro(l_chunks:list, dir:str, filename_init:str):
+def export_spectro(l_chunks:list, specie_name:str, filename:str):
     """ Converts audio into spectros and exports them """
-
-    try:
-        os.chdir('train_set/' + dir)
-    except:
-        os.mkdir('train_set/' + dir) # create a new directory to store spectro
-        os.chdir('train_set/' + dir)
+    Path(f"train_set/{specie_name}").mkdir(parents=True, exist_ok=True)
 
     for idx_chunk, chunk in enumerate(l_chunks):
         spectro = librosa.stft(chunk)
-
         img = librosa.display.specshow(librosa.amplitude_to_db(np.abs(spectro), ref=np.max))
-        path = filename + '_spec_' + str(idx_chunk) + '.png'
-        plt.savefig(path)
-    os.chdir('../..')
+        plt.savefig(f"train_set/{specie_name}/{filename}_spec_{idx_chunk}.png")
 
 
-def load_in_blocks(audio_path:str, frame_size: int=5):
+def load_in_blocks(audio_path:str, frame_size: int=5, limit_chunks:int = 30):
     """Chunks audio into parts of 'frame_size' seconds
 
     Args:
@@ -48,20 +43,13 @@ def load_in_blocks(audio_path:str, frame_size: int=5):
     """
     entire_audio, sr = librosa.core.load(audio_path, mono=True) # to get initial audio duration
     available_time = librosa.get_duration(entire_audio)
-    l_chunks = [0]*int(available_time//frame_size)
+    l_chunks:list[int] = [0 for _ in range(min(int(available_time//frame_size),limit_chunks))] # limite de chargement, en nombre de chunks
 
-    offset = 0 # start reading after this time (in seconds)
-    idx = 0
-    # While audio isn't finished
-    while available_time > frame_size:
-        chunk, sr = librosa.core.load(audio_path, mono=True sr=sr, offset=offset, duration=frame_size)
-        l_chunks[idx] = chunk
-
-        offset += frame_size
-        available_time -= frame_size
-        idx += 1
+    for idx,_ in enumerate(l_chunks):
+        l_chunks[idx],_ = librosa.core.load(audio_path, mono=True, sr=sr, offset=idx*frame_size, duration=frame_size)
 
     return l_chunks
 
-dir = 'birdsong-recognition/brdowl/*'
-list = audio_processing(dir)
+if __name__ == "__main__"
+    dir = 'birdsong-recognition/brdowl/*'
+    list = audio_processing(dir)
