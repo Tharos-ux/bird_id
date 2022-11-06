4d4fe765b1549b666251d8bda82702ee6e070758
tharos <tharos@tcorp.com>
2022-11-06 16:55:34 +0100

    Save and load of models

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 PATH_DATA: str = "data"
 PATH_TRAIN: str = "train_set"
 PATH_UNK: str = "unknown"
-BATCH_SIZE: int = 300  # taille des paquets de données
+BATCH_SIZE: int = 30  # taille des paquets de données
 HEIGHT: int = 480  # nb de pixels en hauteur
 WIDTH: int = 354  # nb de pixels en largeur
 # nombre de packs d'itérations d'entrainements (epocs = k itérations du gradient)
--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,5 @@
 from .audio_sampling import audio_processing
 from .tensorflow_model import modeling
 from .tensorflow_model import prediction
+from .tensorflow_model import save_model
+from .tensorflow_model import load_model
